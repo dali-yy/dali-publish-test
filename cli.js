23188d3f2a76092135915daa6c7db33d4690acff
qunli.zheng <qunli.zheng2@envision-digital.com>
2023-12-13 09:13:25 +0800

    修改为cli

---
+++ cli.js
@@ -0,0 +1,7 @@
+const daliPublishTest = require('./index');
+
+function main() {
+  daliPublishTest();
+}
+
+module.exports = main;
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function daliPublishTest() {
     console.log('dali-test-publish');
 }
 
-export default daliPublishTest;
+module.exports = daliPublishTest;
